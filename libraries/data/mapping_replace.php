59fdf35c5493b13547ace640f63ba841d1df4432
Daniel Berthereau <Daniel.github@Berthereau.net>
2017-06-12 00:00:00 +0200

    Updated upgrade of themes.

--- libraries/data/mapping_replace.php
+++ libraries/data/mapping_replace.php
@@ -12,6 +12,29 @@ $mainNav = <<<'OUTPUT'
 $this->upgrade()->currentSite()->publicNav()->menu()->renderMenu(null, ['maxDepth' => $this->themeSetting('nav_depth') - 1])
 OUTPUT;
 
+$bootstrapTwitterInput = <<<'INPUT'
+$this->addHelperPath(
+    __DIR__
+        . DIRECTORY_SEPARATOR . 'libraries'
+        . DIRECTORY_SEPARATOR . 'Twitter'
+        . DIRECTORY_SEPARATOR . 'View'
+        . DIRECTORY_SEPARATOR . 'Helper',
+    'Twitter_View_Helper_');
+INPUT;
+
+$bootstrapTwitterOutput = <<<'OUTPUT'
+/* // Need to be upgraded for Omeka S.
+$thisView->addHelperPath(
+    __DIR__
+        . DIRECTORY_SEPARATOR . 'libraries'
+        . DIRECTORY_SEPARATOR . 'Twitter'
+        . DIRECTORY_SEPARATOR . 'View'
+        . DIRECTORY_SEPARATOR . 'Helper',
+    'Twitter_View_Helper_');
+*/
+OUTPUT;
+
 return array(
     '$this->upgrade()->public_nav_main()' => $mainNav,
+    $bootstrapTwitterInput => $bootstrapTwitterOutput,
 );
